e2892408d47e266c4792c93f07f8bee050af6a35
Alexander Wilden <alexander.wilden@aixigo.de>
2016-02-17 12:44:03 +0100

    Fixed relative imports.

--- laxar-patterns.js
+++ laxar-patterns.js
@@ -3,15 +3,15 @@
  * Released under the MIT license.
  * http://laxarjs.org/license
  */
-import * as actions from 'lib/actions';
-import * as errors from 'lib/errors';
-import * as flags from 'lib/flags';
-import * as i18n from 'lib/i18n';
-import * as json from 'lib/json';
-import * as patches from 'lib/patches';
-import * as resources from 'lib/resources';
-import * as validation from 'lib/validation';
-import * as visibility from 'lib/visibility';
+import * as actions from './lib/actions';
+import * as errors from './lib/errors';
+import * as flags from './lib/flags';
+import * as i18n from './lib/i18n';
+import * as json from './lib/json';
+import * as patches from './lib/patches';
+import * as resources from './lib/resources';
+import * as validation from './lib/validation';
+import * as visibility from './lib/visibility';
 
 export {
    actions,
